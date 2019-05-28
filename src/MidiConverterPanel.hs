49966933aa0d60fd6d5c5aecff948f05b3b1931e
jarra <suhrawardi@gmail.com>
2019-05-28 21:04:06 +0200

    Refactoring the MidiConverter

--- src/MidiConverterPanel.hs
+++ src/MidiConverterPanel.hs
@@ -21,24 +21,24 @@ convertOct oct midiMessage     = do
     Just (ANote ap k v d)
 
 
-convertMsg :: Int -> Time -> MidiMessage -> Maybe MidiMessage
-convertMsg channel dur (Std (NoteOn c k v))   = Just (ANote channel k v dur)
-convertMsg channel dur (Std (PitchWheel c p)) = Just (Std (PitchWheel channel p))
-convertMsg _       _   _                      = Nothing
+convertMsg :: Int -> MidiMessage -> Maybe MidiMessage
+convertMsg channel (Std (NoteOn c k v))   = Just (Std (NoteOn channel k v))
+convertMsg channel (Std (NoteOff c k v))  = Just (Std (NoteOff channel k v))
+convertMsg channel (Std (PitchWheel c p)) = Just (Std (PitchWheel channel p))
+convertMsg _       _                      = Nothing
 
 
-convert :: Int -> Time -> Octave -> MidiMessage -> Maybe MidiMessage
-convert channel dur oct msg = convertOct oct (convertMsg channel dur msg)
+convert :: Int -> Octave -> MidiMessage -> Maybe MidiMessage
+convert channel oct msg = convertOct oct (convertMsg channel msg)
 
 
 midiConverterPanel :: UISF (Int, Maybe [MidiMessage]) (Maybe [MidiMessage])
 midiConverterPanel = topDown $ setSize (500, 150) $ proc (channel, miM) -> do
     isPlaying <- buttonsPanel >>> handleButtons -< ()
     oct <- title "Octave" $ withDisplay (hiSlider 1 (1, 10) 4) -< ()
-    rec s <- vdelay -< (0.1, fmap (mapMaybe (convert channel 1 oct)) miM')
+    rec s <- vdelay -< (0.1, fmap (mapMaybe (convert channel oct)) miM')
         let miM' = mappend miM s
 
     if isPlaying
       then returnA -< miM'
       else returnA -< Nothing
-
